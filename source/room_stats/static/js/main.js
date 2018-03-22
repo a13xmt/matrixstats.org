32e48cd41062882613595e6b805768a0cfcd93fb
Alexandr Motchany <alexandr.motchany@gmail.com>
2018-03-22 16:33:23 +0300

    Fix room list animations. Add 'copy room id' button to room details.

--- source/room_stats/static/js/main.js
+++ source/room_stats/static/js/main.js
@@ -9,3 +9,30 @@ function search_text_changed(e){
     return false;
   }
 }
+
+$(".room-container").each(function(){
+  $(this).hover(
+    function(){
+      var el = $(this).find('.room-description')
+      var curHeight = el.height()
+      var autoHeight = el.css('height', 'initial').height()
+      if (autoHeight > 56){
+        el.height(curHeight).animate({height: autoHeight}, 300)
+      } else {
+        var curHeight = el.height(56)
+      }
+    },
+    function(){
+      var el = $(this).find('.room-description')
+      el.animate({height: 56}, 300)
+    }
+  );
+})
+
+function copy_room_id(element){
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).attr('data-id')).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
