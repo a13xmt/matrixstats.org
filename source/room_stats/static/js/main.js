2f4bf397203c998f9476d3486d5f44b4d8c0f970
a13xmt <alexandr.motchany@gmail.com>
2018-04-03 17:16:38 +0300

    Fix chart date formatting. Remove legend.

--- source/room_stats/static/js/main.js
+++ source/room_stats/static/js/main.js
@@ -106,25 +106,24 @@ function display_members_graph(data){
   data = data['result']
 
   var svg = dimple.newSvg('.room-graph', '100%', 400);
-  var myChart = new dimple.chart(svg, data);
-  var x = myChart.addTimeAxis('x', 'date', undefined, '%Y-%m');
+  var chart = new dimple.chart(svg, data);
+  var x = chart.addTimeAxis('x', 'date', '%Y-%m-%d', '%Y-%m-%d');
+  x.title = ""
   x.addOrderRule('Date');
   x.timePeriod = d3.timeMonth;
-  x.timeInterval = 2;
+  x.timeInterval = 1;
   x.floatingBarWidth = 1;
-  myChart.addMeasureAxis('y', 'members_count');
-  var s = myChart.addSeries(null, dimple.plot.line);
+  var y = chart.addMeasureAxis('y', 'members_count');
+  y.title = ""
+  var s = chart.addSeries(null, dimple.plot.line);
   s.barGap = 0;
   s.getTooltipText = function (e) {
     return [
-      (new Date(e.x)),
-      e.y
+      (new Date(e.x)).toLocaleDateString("en-us", {year: "numeric", month: "long", day: "numeric"}),
+      e.y + " members"
     ];
   };
-  myChart.draw();
-  myChart.svg.selectAll('.dimple-bar, .dimple-legend-key')
-    .style('stroke', 'none')
-    .style('opacity', '1');
+  chart.draw();
 }
 
 if($('.room-graph').length){
