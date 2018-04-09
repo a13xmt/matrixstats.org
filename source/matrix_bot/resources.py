4211a5f1d78524187505db5598755e6feef86fac
a13xmt <alexandr.motchany@gmail.com>
2018-04-09 19:28:18 +0300

    Add redis to bot resources.

--- source/matrix_bot/resources.py
+++ source/matrix_bot/resources.py
@@ -1,5 +1,12 @@
 import requests
+import os
+import redis
 
 rs = requests.Session()
 rs.headers.update({'User-Agent': 'Matrixbot-dev-experimental/1.1 (+https://matrixstats.org)'})
 
+rds = redis.StrictRedis(
+    host=os.environ.get("REDIS_HOST"),
+    port=os.environ.get("REDIS_PORT", 6379),
+    db=0,
+)
