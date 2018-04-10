d8ff6c681d49cae12668d2a0563dc4e86c7d8cd4
a13xmt <alexandr.motchany@gmail.com>
2018-04-10 19:34:46 +0300

    Add celery_once to dependencies.

--- source/matrix_stats/celery.py
+++ source/matrix_stats/celery.py
@@ -1,29 +1,40 @@
 from __future__ import absolute_import, unicode_literals
 import os
-
 import celery
 import raven
 from raven.contrib.celery import register_signal, register_logger_signal
 
+
 RAVEN_DSN = os.environ.get('RAVEN_DSN')
 # Separate logs by enviroments (dev/prod)
-ENVIRONMENT = os.environ.get('DJANGO_SETTINGS_MODULE').split('.')[-1]
+RAVEN_ENVIRONMENT = os.environ.get('DJANGO_SETTINGS_MODULE').split('.')[-1]
+
+CELERY_BROKER = 'redis://%s:%s/4' % (
+    os.environ.get('REDIS_HOST'),
+    os.environ.get('REDIS_PORT'),
+)
 
 class Celery(celery.Celery):
     def on_configure(self):
         client = raven.Client(
             dsn=RAVEN_DSN,
-            environment=ENVIRONMENT
+            environment=RAVEN_ENVIRONMENT
         )
         register_logger_signal(client)
         register_signal(client)
 
 app = Celery(
     app='matrix_stats',
-    broker='redis://%s:%s/4' % (
-        os.environ.get('REDIS_HOST'),
-        os.environ.get('REDIS_PORT'),
-    )
+    broker=CELERY_BROKER
 )
 
+app.conf.ONCE = {
+  'backend': 'celery_once.backends.Redis',
+  'settings': {
+    'url': CELERY_BROKER,
+    'default_timeout': 60 * 60
+  }
+}
+
 app.autodiscover_tasks(['matrix_bot.tasks'])
+
