ed7297e42283a1f0ccb7b88fc6527182d7147426
a13xmt <alexandr.motchany@gmail.com>
2018-04-10 13:31:59 +0300

    Add Raven handlers to all Celery events.

--- source/matrix_stats/celery.py
+++ source/matrix_stats/celery.py
@@ -1,6 +1,24 @@
 from __future__ import absolute_import, unicode_literals
 import os
-from celery import Celery
+
+import celery
+import raven
+from raven.contrib.celery import register_signal, register_logger_signal
+
+RAVEN_DSN = os.environ.get('RAVEN_DSN')
+# Separate logs by enviroments (dev/prod)
+RELEASE_NAME = os.environ.get('DJANGO_SETTINGS_MODULE').split('.')[-1]
+
+print(RELEASE_NAME, RAVEN_DSN)
+
+class Celery(celery.Celery):
+    def on_configure(self):
+        client = raven.Client(
+            dsn=RAVEN_DSN,
+            release=RELEASE_NAME
+        )
+        register_logger_signal(client)
+        register_signal(client)
 
 app = Celery(
     app='matrix_stats',
