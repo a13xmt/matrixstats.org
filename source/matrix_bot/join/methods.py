9b3be858f8cd710f1c623bc096c2369f5b0ef582
a13xmt <alexandr.motchany@gmail.com>
2018-04-11 19:56:41 +0300

    Add method for joining rooms.

--- source/matrix_bot/join/methods.py
+++ source/matrix_bot/join/methods.py
@@ -1,5 +1,4 @@
-
-def join_room(self, room_id):
+def join(self, room_id):
     response = self.api_call("POST", "/rooms/%s/join" % room_id, json={})
     if response.status_code == 200:
         return response.json().get('room_id')
