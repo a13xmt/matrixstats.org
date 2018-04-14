f6f74f60deebacac50ee4b4fef8b2c588ef54e87
a13xmt <alexandr.motchany@gmail.com>
2018-04-14 17:50:04 +0300

    Add methods for transfering daily statistics from Redis to Postgres.

--- source/matrix_bot/statistics/__init__.py
+++ source/matrix_bot/statistics/__init__.py
@@ -1,1 +1,1 @@
-from .methods import get_unique_messages, get_unique_members, get_active_rooms
+from .methods import get_unique_messages, get_unique_senders, get_active_rooms, save_daily_stats
--- source/matrix_bot/statistics/methods.py
+++ source/matrix_bot/statistics/methods.py
@@ -1,25 +1,128 @@
-def get_daily_messages_count(self, room_id, date):
-    keys = self._scan_keys(
+from room_stats.models import RoomStatisticalData, make_statistical_data_key, get_period_starting_date
+
+def get_unique_messages(self, room_id, datestr):
+    key = self._prefixed(
         "e__m.text__%s__%s" % (
             room_id,
-            date.strftime("%Y-%m-%d*"),
+            datestr
         )
     )
-    p = self.rds.pipeline()
-    for k in keys:
-        p.scard(k)
-    result = p.execute()
-    return sum(result)
+    return (
+        self.rds.scard(key),
+        list([s.decode() for s in self.rds.smembers(key)])
+    )
 
-def get_daily_members_count(self, room_id, date):
-    keys = self._scan_keys(
+def get_unique_senders(self, room_id, datestr):
+    key = self._prefixed(
         "e__c.dm__%s__%s" % (
             room_id,
-            date.strftime("%Y-%m-%d*"),
+            datestr
         )
     )
-    p = self.rds.pipeline()
-    for k in keys:
-        p.scard(k)
-    result = p.execute()
-    return sum(result)
+    return (
+        self.rds.scard(key),
+        list([s.decode() for s in self.rds.smembers(key)])
+    )
+
+def get_active_rooms(self, datestr):
+    key = self._prefixed(
+        "active_rooms__%s" % datestr
+    )
+    return (
+        self.rds.scard(key),
+        list([s.decode() for s in self.rds.smembers(key)])
+    )
+
+
+def save_daily_stats(self, room_id, date):
+    """ Transfer daily statistics from Redis to Postgres.
+    Update room overall statistics based on it.
+    """
+    datestr = date.strftime("%Y-%m-%d")
+    messages_total, messages = get_unique_messages(self, room_id, datestr)
+    senders_total, senders = get_unique_senders(self, room_id, datestr)
+
+    #
+    # Update or create daily statistics record
+    #
+    key =  make_statistical_data_key('d', date, room_id)
+    ds = None
+    try:
+        ds = RoomStatisticalData.objects.get(pk=key)
+    except RoomStatisticalData.DoesNotExist as e:
+        ds = RoomStatisticalData(
+            room_id=room_id,
+            period='d',
+            starts_at=get_period_starting_date('d', date),
+            data={},
+            messages_total=0,
+            senders_total=0
+        )
+    ds.messages_total = messages_total
+    ds.senders_total = senders_total
+    ds.data['messages'] = messages
+    ds.data['senders'] = senders
+    if messages or senders:
+        ds.save()
+
+    #
+    # Update weekly statistics
+    #
+    key = make_statistical_data_key('w', date, room_id)
+    ws = None
+    try:
+        ws = RoomStatisticalData.objects.get(pk=key)
+    except RoomStatisticalData.DoesNotExist as e:
+        ws = RoomStatisticalData(
+            room_id=room_id,
+            period='w',
+            starts_at=get_period_starting_date('w', date),
+            data={},
+            messages_total=0,
+            senders_total=0
+        )
+    # Append unique messages and senders to weekly list
+    weekly_messages = ws.data.get('messages', [])
+    weekly_messages.extend(messages)
+    weekly_messages = list(set(weekly_messages))
+    weekly_senders = ws.data.get('senders', [])
+    weekly_senders.extend(senders)
+    weekly_senders = list(set(weekly_senders))
+
+    ws.data['messages'] = weekly_messages
+    ws.data['senders'] = weekly_senders
+    ws.messages_total = len(weekly_messages)
+    ws.senders_total = len(weekly_senders)
+    if weekly_messages or weekly_senders:
+        ws.save()
+
+    #
+    # Update monthly statistics
+    #
+    key = make_statistical_data_key('m', date, room_id)
+    ms = None
+    try:
+        ms = RoomStatisticalData.objects.get(pk=key)
+    except RoomStatisticalData.DoesNotExist as e:
+        ms = RoomStatisticalData(
+            room_id=room_id,
+            period='m',
+            starts_at=get_period_starting_date('m', date),
+            data={},
+            messages_total=0,
+            senders_total=0
+        )
+    # Append unique messages and senders to monthly list
+    monthly_messages = ms.data.get('messages', [])
+    monthly_messages.extend(messages)
+    monthly_messages = list(set(monthly_messages))
+    monthly_senders = ms.data.get('senders', [])
+    monthly_senders.extend(senders)
+    monthly_senders = list(set(monthly_senders))
+
+    ms.data['messages'] = monthly_messages
+    ms.data['senders'] = monthly_senders
+    ms.messages_total = len(monthly_messages)
+    ms.senders_total = len(monthly_senders)
+    if monthly_senders or monthly_messages:
+        ms.save()
