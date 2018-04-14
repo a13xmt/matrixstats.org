21112b09c4d5931a74aac6d7d4271157c03aeddf
a13xmt <alexandr.motchany@gmail.com>
2018-04-14 20:35:36 +0300

    Fix message handler.

--- source/matrix_bot/sync/handlers/rooms.py
+++ source/matrix_bot/sync/handlers/rooms.py
@@ -1,24 +1,24 @@
 from datetime import datetime
 from .messages import message_event_handlers
 
-def room_message_handler(self, event):
+def room_message_handler(self, event, room_id):
     msg_type = event.get("content", {}).get("msgtype")
     msg_handler = message_event_handlers.get(msg_type)
-    result = msg_handler(self, event) if msg_handler else None
+    result = msg_handler(self, event, room_id) if msg_handler else None
 
 def room_message_feedback_handler(self, event):
     pass
 
-def room_name_handler(self, event):
+def room_name_handler(self, event, room_id):
     pass
 
-def room_topic_handler(self, event):
+def room_topic_handler(self, event, room_id):
     pass
 
-def room_avatar_handler(self, event):
+def room_avatar_handler(self, event, room_id):
     pass
 
-def room_pinned_events_handler(self, event):
+def room_pinned_events_handler(self, event, room_id):
     pass
 
 room_event_handlers = {
