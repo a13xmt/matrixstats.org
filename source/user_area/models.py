fad90698fd1739fa40a668a524044b49678a8c32
a13xmt <alexandr.motchany@gmail.com>
2018-11-02 16:33:03 +0300

    Add Account model.

--- source/user_area/models.py
+++ source/user_area/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.conf import settings
+from room_stats.models import Server
 
-# Create your models here.
+class Account(models.Model):
+    STATUS_CHOICES = (
+        ('p', "Processing"),
+        ('i', "Invited"),
+        ('r', "Registered"),
+        ('e', "Registration Error")
+    )
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='p')
+    registration_code = models.CharField(max_length=36, default='', editable=False)
+    validation_code = models.CharField(max_length=36, default='', editable=False)
+
+    def __str__(self):
+        return self.user.username
+
+
+class BoundServer(models.Model):
+    pass
+    # account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    # homeserver = models.ForeignKey(Server, on_delete=models.CASCADE)
+
+    # dns_validation_code = models.CharField(max_length=255)
