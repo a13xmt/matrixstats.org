1fbd14667279f09eaff2db965068c7d1b2863cb5
a13xmt <alexandr.motchany@gmail.com>
2018-11-04 20:05:47 +0300

    Fix registration appearance.

---
+++ source/user_area/forms.py
@@ -0,0 +1,55 @@
+from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from django_registration import forms as reg_forms
+from django.contrib.auth import forms as auth_forms
+
+from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit, HTML, Div, Button
+from crispy_forms.bootstrap import FormActions
+
+class RegistrationForm(reg_forms.RegistrationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        # self.helper.add_input(Submit('submit', 'Register'))
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-lg-4'
+        self.helper.field_class = 'col-lg-6'
+        self.helper.layout = Layout(
+            Fieldset(
+                '',
+                'username',
+                'email',
+                'password1',
+                'password2',
+            ),
+            FormActions(
+                Submit('submit', 'Register', css_class='btn-block'),
+            )
+        )
+
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+
+class AuthenticationForm(auth_forms.AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        # self.helper.add_input(Submit('submit', 'Login'))
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-lg-4'
+        self.helper.field_class = 'col-lg-6'
+        self.helper.layout = Layout(
+            Fieldset(
+                '',
+                'username',
+                'password'
+            ),
+            FormActions(
+                Submit('submit', 'Login', css_class='btn-block'),
+                Div(template="user_area/widgets/register_links.html")
+            )
+
+        )
